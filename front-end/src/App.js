b8955d9618e0e74cee8126d76bcb88c4c64ebafd
yu-xiu <xiuyutiffany@gmail.com>
2024-05-11 17:35:15 -0700

    update amplify auth

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,18 @@ import HomePage from './pages/HomePage';
 import SingleFileUpload from './pages/SingleFileUpload';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
 import Navbar from './components/NavBar';
+
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
 
 
 function App({signOut, user}) {
+  console.log(user)
   const [selectedFile, setSelectedFile] = useState(null);
   const handleItemClick = (file) => {
     setSelectedFile(file);
@@ -18,7 +25,7 @@ function App({signOut, user}) {
     <Router>
       <Navbar signOut={signOut} user={user}/>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/" element={<HomePage user={user}/>} />
         <Route exact path="/home" element={<HomePage />} />
         <Route exact path="/upload" element={<SingleFileUpload />} />
       </Routes>
@@ -26,4 +33,4 @@ function App({signOut, user}) {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
