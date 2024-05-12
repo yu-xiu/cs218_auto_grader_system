8181416cac492a3c42b1726000bf1169753a3727
yu-xiu <xiuyutiffany@gmail.com>
2024-05-11 17:37:23 -0700

    add amplify auth

---
+++ front-end/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "agfrontend6fd5cee3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
